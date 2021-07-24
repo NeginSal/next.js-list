c97b8650e909c2ed26c23928bfaaaf2f5cc37df1
NeginSal <neginsaljooghi@gmail.com>
2021-07-24 10:42:45 +0430

    linking between pages

--- comps/Navbar.js
+++ comps/Navbar.js
@@ -1,12 +1,15 @@
+import Link from 'next/link'
+
+
 const Navbar = () => {
     return (
         <nav>
             <div className="logo">
                 <h1>Web Shop</h1>
             </div>
-            <a>Home</a>
-            <a>About</a>
-            <a>Products Listing</a>
+            <Link href="/"><a>Home</a></Link>
+            <Link href="/about"><a>About</a></Link>
+            <Link href="/ninjas"><a>Products Listing</a></Link>
         </nav>
      );
 }
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Footer from '../comps/Footer'
 import Navbar from '../comps/Navbar'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
           Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris.
           Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium.
       </p>
+      <Link href="/ninjas"><a>See All Products</a></Link>
       <Footer/>
 
     </div>
