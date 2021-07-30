1fe98f07f3b975597b6ec2ac46b78756e2731fd3
NeginSal <neginsaljooghi@gmail.com>
2021-07-30 17:03:02 +0430

    adding Styles

--- pages/index.js
+++ pages/index.js
@@ -7,13 +7,13 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div>
-      <h1>HomePage</h1>
-      <p> Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro.
+      <h1 className={styles.title}>HomePage</h1>
+      <p className={styles.text}> Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro.
           De carne lumbering animata corpora quaeritis.
           Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris.
           Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium.
       </p>
-      <Link href="/ninjas"><a>See All Products</a></Link>
+      <Link href="/ninjas"><a className={styles.btn}>See All Products</a></Link>
     </div>
 
   )
